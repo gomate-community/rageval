7147bf0f184dc7cd2cc6af0771134f8279c25f3b
faneshion <fanyixing111@hotmail.com>
2024-01-31 17:35:56 +0800

    init tests

---
+++ tests/test_evaluation.py
@@ -0,0 +1,18 @@
+import pytest
+
+import rageval
+
+@pytest.mark.cron
+def test_evaluation():
+    '''
+    This is test unit for testing the load_dataset function.
+    '''
+
+    # 1) init test task: task type, metrics
+
+    # 2) load dataset, and extract testset
+    #train_data = rageval.datasets.load_data('', task='')
+    #assert len(train_data) == 300
+
+    # 3) run evaluation
+    # result = evaluate(testset, info)
