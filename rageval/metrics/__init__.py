a58895b3ea9fc218ffd8dc1ff9ec85eef988f5aa
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-02-23 23:36:52 +0800

    add rouge

--- rageval/metrics/__init__.py
+++ rageval/metrics/__init__.py
@@ -3,3 +3,4 @@ from .base import MetricWithLLM
 from ._context_recall import ContextRecall
 from ._answer_exact_match import AnswerExactMatch
 from ._answer_groudedness import AnswerGroundedness
+from ._answer_rouge import AnswerRouge
