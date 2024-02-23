9fc71d2329822e7b11894f906210ba919a27c4ae
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-02-24 00:55:27 +0800

    fix format

--- rageval/metrics/_answer_rouge.py
+++ rageval/metrics/_answer_rouge.py
@@ -13,8 +13,9 @@ from rageval.metrics import Metric
 class AnswerRouge(Metric):
     """
     Estimates ROUGE score by estimating answer and groundtruth answers.
-    ROUGE is case insensitive, so the input text is converted to lower case before computing the score.
-    
+
+    ROUGE is case insensitive, so the input text is converted to lower case before computing the score.This metrics is a wrapper around the https://github.com/google-research/google-research/blob/master/rouge/rouge_scorer.py
+
     Attributes
     ----------
     name : str
@@ -23,9 +24,7 @@ class AnswerRouge(Metric):
         "rouge2": bigram (2-gram) based scoring
         "rougeL": Longest common subsequence based scoring.
         "rougeLSum": splits text using "\n"
-    tokenizer : Callable, optional, for non-latin languages, a tokenizer can be passed to the scorer. For example, the `jieba.cut` can be used for Chinese.   
-
-    This metrics is a wrapper around the https://github.com/google-research/google-research/blob/master/rouge/rouge_scorer.py
+    tokenizer : Callable, optional, for non-latin languages, a tokenizer can be passed to the scorer. For example, the `jieba.cut` can be used for Chinese.
 
     """
 
@@ -36,25 +35,12 @@ class AnswerRouge(Metric):
         self.rouge_type = rouge_type
         self.scorer = rouge_scorer.RougeScorer([rouge_type], use_stemmer=True, tokenizer=tokenizer)
 
-    def _compute_one(
-        self,
-        answer: str,
-        gt_answers: List[str]
-    ) -> float:
-        """
-        Evaluate the ROUGE between a single answer and groundtruth answers.
-        """
+    def _compute_one(self, answer: str, gt_answers: List[str]) -> float:
+        """Evaluate the ROUGE between a single answer and groundtruth answers."""
         score = self.scorer.score_multi(gt_answers, answer)
         return score[self.rouge_type].fmeasure
 
-    def _compute_batch(
-        self,
-        dataset: Dataset
-    ) -> list:
-        """
-        Evaluate the ROUGE of a batch of answers.
-        """
-
+    def _compute_batch(self, dataset: Dataset) -> list:
+        """Evaluate the ROUGE of a batch of answers."""
         results = [self._compute_one(answer, gt_answer) for answer, gt_answer in zip(dataset["answers"], dataset["gt_answers"])]
-        
         return results
