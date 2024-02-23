73039146def3293cf4685c8a62e003b421bf0b82
QianHaosheng <qianhaosheng1027@foxmail.com>
2024-02-23 14:26:27 +0800

    add answer exact match metric

--- rageval/metrics/__init__.py
+++ rageval/metrics/__init__.py
@@ -1,4 +1,5 @@
 from .base import Metric
 from .base import MetricWithLLM
 from ._context_recall import ContextRecall
+from ._answer_exact_match import AnswerExactMatch
 from ._answer_groudedness import AnswerGroundedness
