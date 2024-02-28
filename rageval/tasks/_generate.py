dba33b3ee8050dd685f73e78ffb301d6de068ed2
Wenshansilvia <ww2468@columbia.edu>
2024-02-28 19:58:43 +0800

    update typing List

--- rageval/tasks/_generate.py
+++ rageval/tasks/_generate.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from typing import Union
+from typing import Union, List
 
 from rageval.metrics import Metric
 from rageval.tasks import BaseTask
@@ -10,7 +10,7 @@ class Generate(BaseTask):
     # Define required columns in testset for the evaluation of the task
     required_columns = ['questions', 'answers', 'gt_answers']
 
-    def __init__(self, metrics: Union[str, list[str], list[Metric]]):
+    def __init__(self, metrics: Union[str, List[str], List[Metric]]):
         """Init task"""
 
         super().__init__(metrics)
