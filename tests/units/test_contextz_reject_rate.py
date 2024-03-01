04b3c284423b791c3063b85643ecf576b875fc3e
zhangzhihao <zhangzhihao22s@ict.ac.cn>
2024-03-01 16:46:33 +0800

    add RR test file

---
+++ tests/units/test_contextz_reject_rate.py
@@ -0,0 +1,43 @@
+import pytest
+from datasets import Dataset
+
+from rageval.metrics import ContextRejectRate
+
+
+@pytest.fixture(scope='module')
+def sample():
+    test_case = {
+        "questions": [
+            "Why did Bushnell set himself on fire?",
+            "Did Bushnell have a wife?"
+        ],
+        "contexts": [
+            [
+                ["An active-duty member of the U.S. Air Force has died after he set himself ablaze outside the Israeli Embassy in Washington, D.C., while declaring that he “will no longer be complicit in genocide.”"],
+                ["The 25-year-old airman, Aaron Bushnell, of San Antonio, Texas, died from his injuries, the Metropolitan Police Department said Monday."],
+                ["Bushnell had walked up to the embassy shortly before 1 p.m. Sunday and began livestreaming on the video streaming platform Twitch, a person familiar with the matter told The Associated Press. Law enforcement officials believe he set his phone down and then doused himself in accelerant and ignited the flames. At one point, he said he “will no longer be complicit in genocide,” the person said. The video was later removed from the platform, but law enforcement officials have obtained and reviewed a copy."]
+            ],
+            [
+                ["An active-duty member of the U.S. Air Force has died after he set himself ablaze outside the Israeli Embassy in Washington, D.C., while declaring that he “will no longer be complicit in genocide.”"],
+                ["The 25-year-old airman, Aaron Bushnell, of San Antonio, Texas, died from his injuries, the Metropolitan Police Department said Monday."],
+                ["Bushnell had walked up to the embassy shortly before 1 p.m. Sunday and began livestreaming on the video streaming platform Twitch, a person familiar with the matter told The Associated Press. Law enforcement officials believe he set his phone down and then doused himself in accelerant and ignited the flames. At one point, he said he “will no longer be complicit in genocide,” the person said. The video was later removed from the platform, but law enforcement officials have obtained and reviewed a copy."]
+            ]
+        ]
+    }
+    return test_case
+
+
+@pytest.fixture(scope='module')
+def testset(sample):
+    ds = Dataset.from_dict(sample)
+    return ds
+
+
+def test_case_on_answer_exact_match(testset):
+    metric = ContextRejectRate()
+    assert metric.name == "context_reject_rate"
+    assert metric.homepage == ""
+    assert metric.mtype == 'ContextRejectRate'
+    score, results = metric.compute(testset, 1)
+    assert 0 <= score <= 1
+    assert isinstance(results, Dataset)
