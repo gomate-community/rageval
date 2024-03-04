a794079a92591ea84bba5471043c2fb0f7d439b2
QianHaosheng <qianhaosheng1027@foxmail.com>
2024-03-04 19:06:11 +0800

    add citation recall metric

--- rageval/metrics/__init__.py
+++ rageval/metrics/__init__.py
@@ -1,7 +1,6 @@
 from .base import Metric, MetricWithLLM, add_attribute
 from ._context_recall import ContextRecall
-from ._answer_rouge_correctness import AnswerRougeCorrectness
 from ._context_reject_rate import ContextRejectRate
 from ._answer_exact_match import AnswerEMCorrectness
 from ._answer_claim_recall import AnswerNLICorrectness
-from ._answer_f1 import AnswerF1Correctness
+from ._answer_citation_recall import AnswerCitationRecall
