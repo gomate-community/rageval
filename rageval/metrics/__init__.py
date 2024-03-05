47a9da2e8c4f688acad1babb9c19c1b629a43a20
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-03-05 16:08:16 +0800

    add disambig f1 score

--- rageval/metrics/__init__.py
+++ rageval/metrics/__init__.py
@@ -3,5 +3,4 @@ from ._context_recall import ContextRecall
 from ._context_reject_rate import ContextRejectRate
 from ._answer_exact_match import AnswerEMCorrectness
 from ._answer_claim_recall import AnswerNLICorrectness
-from ._answer_citation_recall import AnswerCitationRecall
-from ._answer_citation_precision import AnswerCitationPrecision
+from ._answer_disambig_f1 import AnswerDisambigF1Correctness
