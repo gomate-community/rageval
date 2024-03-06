500a49463341b824008574836c3d1b8343831d60
FBzzh <13754699561@163.com>
2024-03-06 20:06:22 +0800

    add answer_bleu.py

--- rageval/metrics/__init__.py
+++ rageval/metrics/__init__.py
@@ -3,3 +3,4 @@ from ._context_recall import ContextRecall
 from ._context_reject_rate import ContextRejectRate
 from ._answer_exact_match import AnswerEMCorrectness
 from ._answer_claim_recall import AnswerNLICorrectness
+from ._answer_bleu import AnswerBleuScore
