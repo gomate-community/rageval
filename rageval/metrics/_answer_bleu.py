28164a8a48bd22b5bb3c313b1bbc9533cd753e63
FBzzh <13754699561@163.com>
2024-03-07 17:12:25 +0800

    change bleu_compute, add bleu_test file

--- rageval/metrics/_answer_bleu.py
+++ rageval/metrics/_answer_bleu.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 
 import datasets
+from datasets import Dataset
 import re
 
 from rageval.metrics import Metric, add_attribute
 
 
 _DESCRIPTION = """\
-BLEU (Bilingual Evaluation Understudy) is an algorithm for evaluating the quality of text which has been machine-translated from one natural language to another. 
-Scores are calculated for individual translated segments—generally sentences—by comparing them with a set of good quality reference translations. 
-Those scores are then averaged over the whole corpus to reach an estimate of the translation's overall quality. 
+BLEU (Bilingual Evaluation Understudy) is an algorithm for evaluating the quality of text which has been machine-translated from one natural language to another.
+Scores are calculated for individual translated segments—generally sentences—by comparing them with a set of good quality reference translations.
+Those scores are then averaged over the whole corpus to reach an estimate of the translation's overall quality.
 Neither intelligibility nor grammatical correctness are not taken into account.
 
 For details, see the paper: http://www.aclweb.org/anthology/P02-1040.pdf
@@ -26,8 +27,37 @@ Optional Args:
 
 Functions:
     _clean: clean special word in sentence.
+    _compute_single: compute bleu score for single prediction with its references
+
+Examples:
+    >>> from datasets import Dataset
+    >>> import rageval as rl
+    >>> sample = {
+    ...     "answers": [
+    ...         "Hello, world!",
+    ...         "I am a metric named bleu."
+    ...     ],
+    ...     "gt_answers": [
+    ...         [
+    ...             "Hello, my world!",
+    ...         ],
+    ...         [
+    ...             "I am a metric named bleu.",
+    ...             "I am bleu metric.",
+    ...         ]
+    ...     ],
+    ... }
+    >>> dataset = Dataset.from_dict(sample)
+    >>> metric = rl.metrics.AnswerBleuScore()
+    >>> metric.mtype
+    'AnswerCorrectness'
+    >>> s, ds = metric.compute(dataset, batch_size=1)
+    >>> assert 0 <= s <= 1
+    >>> type(ds)
+    <class 'datasets.arrow_dataset.Dataset'>
 
 """
+
 
 _CITATION = """\
 @misc{Kishore2002bleu,
@@ -76,14 +106,14 @@ class AnswerBleuScore(Metric):
         )
 
     def _clean(self, sentence: str, subword: str) -> str:
-        """clean special word in sentence"""
+        """Clean special word in sentence"""
 
         sentence = sentence.strip()
         if subword is not None:
             sentence = re.sub(subword, "", sentence)
         return sentence
 
-    def _compute_batch(self, dataset: datasets.Dataset) -> List[float]:
+    def _compute_single(self, dataset: datasets.Dataset) -> List[float]:
         """Compute the bleu score of a batch of answers."""
 
         scores = []
@@ -97,8 +127,36 @@ class AnswerBleuScore(Metric):
                 gt_answer_clean = self._clean(gt_answer, None)
                 reference = []
                 reference.append(gt_answer_clean.split(' '))
-                references.append(reference)
-            bleu_score, _, _, _, _, _ = bleu.compute(predictions=predictions, references=references)
+            references.append(reference)
+            bleu_result = bleu.compute(predictions=predictions, references=references)
+            bleu_score = bleu_result['bleu']
             scores.append(bleu_score)
 
         return scores
+
+    def compute(
+        self,
+        dataset: Dataset,
+        batch_size: int = None,
+    ) -> Tuple[float, Dataset]:
+        """Evaluate the dataset."""
+
+        bleu = datasets.load_metric("bleu")
+        predictions = []
+        references = []
+        reference = []
+        for output, gt_answers in zip(dataset["answers"], dataset["gt_answers"]):
+            output_clean = self._clean(output, None)
+            predictions.append(list(output_clean.split(' ')))
+            for gt_answer in gt_answers:
+                gt_answer_clean = self._clean(gt_answer, None)
+                reference.append(list(gt_answer_clean.split(' ')))
+            references.append(reference)
+        bleu_result = bleu.compute(predictions=predictions, references=references)
+        bleu_score = bleu_result['bleu']
+        scores = self._compute_single(dataset)
+
+        return bleu_score, dataset.add_column(f"{self.name}", scores)
+
+    def _compute_batch(self, dataset: Dataset) -> list:
+        pass
