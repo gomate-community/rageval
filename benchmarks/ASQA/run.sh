c14fc51342cb272f58185240e5c740fe3c858e06
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-03-28 16:19:19 +0800

    update asqa readme

--- benchmarks/ASQA/run.sh
+++ benchmarks/ASQA/run.sh
@@ -1,17 +1,5 @@
 rageval_dir=$(dirname $(dirname $(dirname $(realpath $0))))
-output_dir="benchmarks/ASQA/output"
 cd $rageval_dir
 echo "Running ASQA Benchmark"
-if [ -z "$(ls -A benchmarks/ASQA/output)" ]; then
-    echo "Generating ASQA examples"
-    python3 $rageval_dir/benchmarks/ASQA/generate.py \
-        --max_num_examples 500 \
-        --max_new_tokens 256 \
-        --output_dir $output_dir \
-        --model "gpt-3.5-turbo-instruct" \
-        --api_key "YOUR_API_KEY"
-        --dataset_name "asqa"
-fi
-echo "Running Evaluation"
-python3 $rageval_dir/benchmarks/ASQA/asqa_benchmark.py --output_dir $output_dir
+python3 benchmarks/ASQA/asqa_benchmark.py --output_dir "benchmarks/ASQA/output" --dataset_path "benchmarks/ASQA/data/gpt-3.5-turbo-instruct.jsonl"
 echo "ASQA Benchmark Complete"
