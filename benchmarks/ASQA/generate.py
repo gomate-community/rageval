8b6977044bc3460e1268d6e4470df09a8a256637
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-03-28 17:33:58 +0800

    move instructGPT to openaiLLM

--- benchmarks/ASQA/generate.py
+++ benchmarks/ASQA/generate.py
@@ -3,91 +3,17 @@ import re
 from rageval.models import OpenAILLM
 import os
 from typing import List, Dict, Optional
-import openai
 import logging
-from tqdm import tqdm
-from langchain.schema import Generation, LLMResult
 import argparse
 
 from prompts import (FEW_SHOT_EXAMPLES, PROMPT)
 
-logger = logging.getLogger(__name__)
-
-class InstructGPT(OpenAILLM):
-    #TODO: move this to the openai.py file and refactor the OpenAILLM base class
-    def __init__(self, model: str = "gpt-3.5-turbo-instruct", *args, **kwargs):
-        super().__init__(model, *args, **kwargs)
-        
-    def generate(self, prompt: str, ):
-        try:
-            response = self.llm.with_options(
-                max_retries=self.num_retries,
-                timeout=self.timeout).completions.create(
-                    model=self.model,
-                    prompt=prompt,
-                    max_tokens=self.max_tokens, # defaults to 16 for openai.completions.create
-                    n=self.n,
-                    temperature=self.temperature,
-                    top_p=self.top_p)
-            result = self.create_llm_result(response)
-            return result
-        except openai.APIConnectionError as e:
-            logger.info("The server could not be reached")
-            logger.info(e.__cause__)  # an underlying Exception, likely raised within httpx.
-            raise e
-        except openai.RateLimitError as e:
-            logger.info("A 429 status code was received; we should back off a bit.")
-            raise e
-        except openai.APIStatusError as e:
-            logger.info("Another non-200-range status code was received")
-            logger.info(e.status_code)
-            logger.info(e.response)
-            raise e
-    
-    def batch_generate(self, prompts: List[str]) -> List[LLMResult]:
-        results = []
-        for prompt in tqdm(prompts):
-            try: 
-                result = self.generate(prompt)
-            except Exception as e:
-                result = LLMResult(generations=[[Generation(text="")]], llm_output={})
-                print(e)
-            results.append(result)
-        return results
-    
-    def create_llm_result(self, response) -> LLMResult:
-        """Create the LLMResult from the choices and prompts."""
-        if not isinstance(response, dict):
-            response = response.model_dump()
-
-        # token Usage
-        token_usage = response.get("usage", {})
-        self.usage["prompt_tokens"] += token_usage.get("prompt_tokens", 0)
-        self.usage["completion_tokens"] += token_usage.get("completion_tokens", 0)
-        self.usage["total_tokens"] += token_usage.get("total_tokens", 0)
-        llm_output = {
-            "token_usage": token_usage,
-            "model_name": self.model,
-            "system_fingerprint": response.get("system_fingerprint", "")
-        }
-
-        choices = response["choices"]
-        generations = [
-            Generation(
-                text=choice["text"],
-                generation_info=dict(
-                    finish_reason=choice.get("finish_reason"),
-                    logprobs=choice.get("logprobs"),
-                ),
-            )
-            for choice in choices
-        ]
-        return LLMResult(generations=[generations], llm_output=llm_output)
-
 def extract_key_information(pred: str) -> str:
     '''Extract key information from the response.'''
-    prefix_to_remove=['interpretations: (.*)$',
-                      'interpretation: (.*)$']
+    prefix_to_remove=['The answers to all interpretations are\: (.*)$',
+                    'The answer to this interpretation is\: (.*)$',
+                    'The answer to this interpretation is (.*)$',
+                    'The answer to the first interpretation is: (.*)$']
     for pattern in prefix_to_remove:
         pred = pred.strip().split('\n\n', 1)[0].strip()
         find = re.compile(pattern).search(pred)
@@ -99,7 +25,7 @@ def extract_key_information(pred: str) -> str:
     pred = re.sub(r'\(\d+\)\s', '', pred) # remove the index numbers
     return pred
 
-def generate_answers(engine: InstructGPT, dataset: Dataset) -> Dataset:
+def generate_answers(engine: OpenAILLM, dataset: Dataset) -> Dataset:
     prompts = [
         PROMPT.format(few_shot_examples=FEW_SHOT_EXAMPLES, 
                       question=data['ambiguous_question'])
@@ -109,15 +35,18 @@ def generate_answers(engine: InstructGPT, dataset: Dataset) -> Dataset:
     response_texts = [r.generations[0][0].text for r in responses]
     answers = [extract_key_information(response) for response in response_texts]
     dataset = dataset.add_column("responses", response_texts)
+    dataset = dataset.add_column("model", [engine.model]*len(dataset))
     return dataset.add_column("answers", answers)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--max_num_examples", type=int, default=5)
+    # parser.add_argument("--max_num_fewshots", type=int, default=8)
     parser.add_argument("--max_new_tokens", type=int, default=256)
-    parser.add_argument("--output_path", type=str, default="benchmarks/ASQA/output")
+    parser.add_argument("--output_dir", type=str, default="benchmarks/ASQA/output")
     parser.add_argument("--model", type=str, default="gpt-3.5-turbo-instruct")
     parser.add_argument("--api_key", type=str, default=None)
+    parser.add_argument("--dataset_name", type=str, default="asqa")
 
     args = parser.parse_args()
 
@@ -127,15 +56,14 @@ if __name__ == "__main__":
 
     print("Init ASQA dataset...")
     os.environ['OPENAI_API_KEY'] = args.api_key
-    engine = InstructGPT(args.model, 
+    engine = OpenAILLM(args.model, 
                          _api_key_env_var = 'OPENAI_API_KEY', 
                          max_tokens=args.max_new_tokens)
 
     print("Start generate answers...")
     dataset = generate_answers(engine, dataset)
 
-    file_path = os.path.join(args.output_path, f"{args.model}.jsonl")
-    dataset.to_json(file_path)
-    print(f"\nFinish generate dataset. Dataset saved as {file_path}")
+    dataset.to_json(f"{args.output_dir}/{args.dataset_name}.jsonl")
+    print(f"\nFinish generate dataset. Dataset saved as {args.output_dir}/{args.dataset_name}.jsonl")
 
     engine.calculate_api_cost()
