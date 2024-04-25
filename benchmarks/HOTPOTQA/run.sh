9a67597033bc1f48375c76351fe67d6b0aa5556e
LittleSunshineQi <bsq13755391243@163.com>
2024-04-25 10:39:25 +0800

    add hotpot_qa benchmark

--- benchmarks/HOTPOTQA/run.sh
+++ benchmarks/HOTPOTQA/run.sh
@@ -5,7 +5,7 @@ echo "Running HotPotQA Benchmark"
 
 python3 benchmarks/HOTPOTQA/hotpot_qa_benchmark.py --output_dir "benchmarks/HOTPOTQA" --remote_split "gpt_3.5_turbo"
 
-# 检查返回状态码
+# Check return status code
 if [ $? -eq 0 ]; then
     echo "HotPotQA Benchmark Complete"
 else
