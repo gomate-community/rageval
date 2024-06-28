da63a18dddfc21c83f129925c75408e61a234feb
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-06-28 17:09:01 +0800

    add bert score

---
+++ rageval/metrics/_answer_bert_score.py
@@ -0,0 +1,130 @@
+import re
+from dataclasses import dataclass
+from typing import List, Tuple
+
+import datasets
+from rageval.metrics import Metric, add_attribute
+from bert_score import BERTScorer
+import logging
+import transformers
+transformers.tokenization_utils.logger.setLevel(logging.ERROR)
+transformers.configuration_utils.logger.setLevel(logging.ERROR)
+transformers.modeling_utils.logger.setLevel(logging.ERROR)
+
+_DESCRIPTION = """\
+BERTScore leverages the pre-trained contextual embeddings from BERT and matches words in candidate and reference sentences by cosine similarity. It has been shown to correlate with human judgment on sentence-level and system-level evaluation. Moreover, BERTScore computes precision, recall, and F1 measure, which can be useful for evaluating different language generation tasks.
+
+For details, see the paper: https://openreview.net/forum?id=SkeHuCVFDr
+"""
+
+_KWARGS_DESCRIPTION = """\
+Args:
+    name : str
+    lang : str, Language of the text. Default is "en".
+    rescale_with_baseline : bool, Whether to rescale the score with pre-computed baseline. Not affect BERTScore's correlation with human judgment. Default is False. For more details, see https://github.com/Tiiiger/bert_score/blob/master/journal/rescale_baseline.md
+    batch_size : int, Batch size for pred and ref pairs.
+
+Optional Args:
+    None
+
+Functions:
+    _clean: clean special word in sentence.
+    _compute_single: compute bleu score for single prediction with its references
+
+Examples:
+    >>> from datasets import Dataset
+    >>> import rageval as rl
+    >>> sample = {
+    ...     "answers": [
+    ...         "It is a guide to action which ensures that the military always obeys the commands of the party.",
+    ...         "It is to insure the troops forever hearing the activity guidebook that party direct.",
+    ...     ],
+    ...     "gt_answers": [
+    ...         [
+    ...             "It is a guide to action that ensures that the military will forever heed Party commands.",
+    ...             "It is the guiding principle which guarantees the military forces always being under the command of the Party.",
+    ...             "It is the practical guide for the army always to heed the directions of the party.",
+    ...         ],
+    ...         [
+    ...             "It is a guide to action that ensures that the military will forever heed Party commands.",
+    ...             "It is the guiding principle which guarantees the military forces always being under the command of the Party.",
+    ...             "It is the practical guide for the army always to heed the directions of the party.",
+    ...         ]
+    ...     ],
+    ... }
+    >>> dataset = Dataset.from_dict(sample)
+    >>> metric = rl.metrics.AnswerBERTScore(lang='en', rescale_with_baseline=True)
+    >>> metric.mtype
+    'AnswerCorrectness'
+    >>> score, results = metric.compute(dataset["answers"], dataset["gt_answers"], 1)
+    >>> score
+    0.5511645078659058
+    >>> results[0]
+    0.7265544533729553
+"""
+
+
+_CITATION = """\
+@inproceedings{bert-score,
+    title={BERTScore: Evaluating Text Generation with BERT},
+    author={Tianyi Zhang* and Varsha Kishore* and Felix Wu* and Kilian Q. Weinberger and Yoav Artzi},
+    booktitle={International Conference on Learning Representations},
+    year={2020},
+    url={https://openreview.net/forum?id=SkeHuCVFDr}
+}
+"""
+
+
+@dataclass
+@add_attribute('mtype', 'AnswerCorrectness')
+@datasets.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
+class AnswerBERTScore(Metric):
+    """BERTScore depends on the model and language pair selected."""
+
+    name = "answer_bert_score"
+
+    ALIAS = ['answer_bert_score']
+
+    def __init__(self, lang: str = "en", rescale_with_baseline=False):
+        """Explicitly initialize the AnswerBERTScore to ensure all parent class initialized."""
+        super().__init__()
+        self.scorer = BERTScorer(lang=lang, rescale_with_baseline=rescale_with_baseline)
+
+    def __repr__(self) -> str:
+        """:return: Formatted string representation of the metric."""
+        return f"{self.ALIAS[0]}"
+
+    def _info(self):
+        return datasets.MetricInfo(
+            description=_DESCRIPTION,
+            inputs_description=_KWARGS_DESCRIPTION,
+            citation=_CITATION,
+            homepage="",
+            features=datasets.Features(
+                {
+                    "answers": datasets.Value("string"),
+                    "gt_answers": datasets.Sequence(datasets.Value("string"))
+                }
+            ),
+            codebase_urls=[
+                "https://github.com/Tiiiger/bert_score/tree/master",
+            ],
+            reference_urls=["https://openreview.net/forum?id=SkeHuCVFDr"]
+        )
+
+    def _compute_single(
+        self,
+        pred_answers: str,
+        ref_answers: List[str]
+    ) -> float:
+        """Compute the BERTscore for a pair of predictions and references."""
+        P, R, F1 = self.scorer.score([pred_answers] * len(ref_answers), ref_answers)
+        return F1.max().tolist()
+
+    def _compute_batch(
+        self,
+        pred_answers: List[str],
+        ref_answers: List[List[str]]
+    ) -> List[float]:
+        """Compute the BERTscore for a batch of predictions and references."""
+        return [self._compute_single(pred, refs) for pred, refs in zip(pred_answers, ref_answers)]
