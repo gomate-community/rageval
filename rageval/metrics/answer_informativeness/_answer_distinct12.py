bc8d00a38c739b6ca1763eb2bdefd7860c56b33c
bugtig6351 <xianning17@mails.ucas.ac.cn>
2024-09-24 22:36:50 +0800

    delete ppl, fix distinct

---
+++ rageval/metrics/answer_informativeness/_answer_distinct12.py
@@ -0,0 +1,125 @@
+from collections import Counter
+from dataclasses import dataclass
+from typing import List, Optional, Iterable
+import datasets
+from nltk import ngrams
+from rageval.metrics import Metric, add_attribute
+
+_DESCRIPTION = """\
+Distinct 1/2 measures the diversity of generated text by calculating the ratio of unique n-grams to the total number of n-grams.
+"""
+
+_KWARGS_DESCRIPTION = """\
+Args:
+    pred_answers (list of str): List of generated texts for which distinct metrics are computed.
+
+Returns:
+    dict: Dictionary containing Distinct-1 and Distinct-2 scores.
+
+Examples:
+    >>> from datasets import Dataset
+    >>> import rageval as rl
+    >>> sample = {
+    ...     "answers": [
+    ...         "This is the first sentence.",
+    ...         "This is the second sentence."
+    ...     ]
+    ... }
+    >>> dataset = Dataset.from_dict(sample)
+    >>> metric = rl.metrics.AnswerDistinct()
+    >>> metric.mtype
+    'AnswerInformativeness'
+    >>> scores = metric.compute(dataset['answers'])
+    >>> scores['distinct_1']
+    0.8
+    >>> scores['distinct_2']
+    0.6
+"""
+
+_CITATION = """\
+@misc{selfmemory2023,
+    title={Lift Yourself Up: Retrieval-augmented Text Generation with Self Memory},
+    author={Xin Cheng and Di Luo and Xiuying Chen and Lemao Liu and Dongyan Zhao and Rui Yan},
+    year={2023},
+    eprint={2305.02437},
+    archivePrefix={arXiv},
+    primaryClass={cs.CL}
+}
+"""
+
+
+def get_distinct_score(pred_answers: List[str], n_grams:int) -> dict:
+    """Compute Distinct-1 and Distinct-2 metrics."""
+    unigram_counter = Counter()
+    bigram_counter = Counter()
+
+    for answer in pred_answers:
+        tokens = answer.split()
+        unigram_counter.update(tokens)
+        bigram_counter.update(ngrams(tokens, 2))
+
+    distinct_1 = len(unigram_counter) / sum(unigram_counter.values())
+    distinct_2 = len(bigram_counter) / sum(bigram_counter.values())
+
+    return {"distinct_1": distinct_1, "distinct_2": distinct_2}
+
+
+@dataclass
+@add_attribute('mtype', 'AnswerInformativeness')
+@datasets.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
+class AnswerDistinct(Metric):
+    """Distinct 1/2 metric for text generation."""
+
+    name = "answer_distinct"
+
+    ALIAS = ['answer_distinct']
+
+    def __init__(self, n_grams: int = 1):
+        """
+        Explicitly initialize Distinct.
+
+        Ensure all parent classes are initialized.
+        """
+        super().__init__()
+        self.n_grams = n_grams
+
+    def __repr__(self) -> str:
+        """:return: Formatted string representation of the metric."""
+        return f"{self.ALIAS[0]}"
+
+    def _info(self):
+        return datasets.MetricInfo(
+            description=_DESCRIPTION,
+            inputs_description=_KWARGS_DESCRIPTION,
+            citation=_CITATION,
+            features=datasets.Features(
+                {
+                    "pred_answers": datasets.Value("string"),
+                }
+            ),
+            codebase_urls=["https://github.com/Hannibal046/SelfMemory/blob/58d8b611ad51605091c7555c0f32dce6702dadbf/src/utils/metrics_utils.py"],
+            reference_urls=["https://arxiv.org/abs/2305.02437"]
+        )
+
+    def _validate_data(
+        self, 
+        pred_answers: Optional[Iterable] = None,
+        ref_answers: Optional[Iterable] = None,
+    ) -> bool:
+        """Validate the input data."""
+        assert isinstance(pred_answers, str) or isinstance(pred_answers, list)
+
+    def _compute_one(
+        self,
+        pred_answers: str,
+    ) -> dict:
+        """Compute Distinct-1 and Distinct-2 metrics for a single text."""
+        return get_distinct_score([pred_answers])
+
+    def _compute_batch(
+        self,
+        pred_answers: List[str],
+        ref_answers: Optional[Iterable] = None,
+    ) -> dict:
+        """Compute Distinct-1 and Distinct-2 metrics for a batch of texts."""
+        return get_distinct_score(pred_answers)
